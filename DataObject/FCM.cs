64b7ce43368255a5c0d2076e374dd8b52467c7bc
praveent696 <praveent696@gmail.com>
2017-02-01 18:18:17 +0530

    FCMSharp second Commit

---
+++ DataObject/FCM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCMSharp.DataObject
+{
+    public class FCMConfig
+    {
+        public string ApplicationID { get; set; }
+        public string SenderID { get; set; }
+    }
+    public class FCMDevice
+    {
+        public string DeviceToken { get; set; }
+        public string Message { get; set; }
+        public string Priority { get; set; }
+        public string Title { get; set; }
+    }
+}
